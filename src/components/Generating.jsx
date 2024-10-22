e11af5296b9de42b56cfe8fae6466aaeed6a8247
mauassenco <mau.assenco@gmail.com>
2024-10-21 21:42:09 -0300

    AssencoAi lp project

---
+++ src/components/Generating.jsx
@@ -0,0 +1,17 @@
+import { loading } from "../assets";
+
+// eslint-disable-next-line react/prop-types
+const Generating = ({ className }) => {
+  return (
+    <div
+      className={`flex h-[3.5rem] items-center rounded-[1.7rem] bg-n-8/80 px-6 ${
+        className || ""
+      } text-base`}
+    >
+      <img className="mr-4 h-5 w-5" src={loading} alt="Loading" />
+      AI is generating
+    </div>
+  );
+};
+
+export default Generating;
